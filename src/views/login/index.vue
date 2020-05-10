a9c54891432f4224e4ea47deb4b36358610ade70
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2020-05-10 17:16:16 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="main-area">
+      <el-card shadow="always">
+        <el-tabs v-model="activeTab" stretch @tab-click="handleClick">
+          <el-tab-pane label="用户登录" name="login">
+            <login />
+          </el-tab-pane>
+          <el-tab-pane label="用户注册" name="register">用户注册</el-tab-pane>
+        </el-tabs>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import login from "./login";
+export default {
+  name: "WelcomePage",
+  components: {
+    login
+  },
+  data() {
+    return {
+      activeTab: "login"
+    };
+  },
+  watch: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+// $bg: #2d3a4b;
+// $dark_gray: #889aa4;
+// $light_gray: #eee;
+.container {
+  min-height: 100%;
+  width: 100%;
+  // background-color: $bg;
+  overflow: hidden;
+  .main-area {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 60px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+}
+</style>
