edd3b6b52ae3c3fec918441403dab7719f46e985
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2020-05-15 17:10:03 +0800

    增加table

--- src/views/user-management/index.vue
+++ src/views/user-management/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="user-container">
-    <el-card shadow="always">
-      <el-tabs :tab-position="tabPosition" style="height: auto">
-        <el-tab-pane
-          v-for="p of tabPaneList"
-          :key="p"
-          v-permission="p.acl"
-          :label="p.label"
-        >{{ p.label }}</el-tab-pane>
+    <el-card shadow="always" style="min-height: 320px;">
+      <el-tabs :tab-position="tabPosition" style="height: 100%">
+        <el-tab-pane v-for="p of tabPaneList" :key="p" v-permission="p.acl" :label="p.label">
+          <div style="margin: 10px 10px;">
+            <el-table :data="tableData" border style="width: 100%;">
+              <el-table-column
+                v-for="c of defaultColumn"
+                :key="c.value"
+                :fixed="c.fixed"
+                :prop="c.value"
+                :label="c.label"
+                :width="c.width || 120"
+              />
+              <el-table-column fixed="right" label="操作" width="150">
+                <template slot-scope="scope">
+                  <el-button size="small" type="text" @click="handleClick(scope.row)">查看</el-button>
+                  <el-button type="text" size="small">编辑</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+        </el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -32,7 +46,40 @@ export default {
           label: "成员管理",
           acl: ["super_admin", "admin"]
         }
-      ]
+      ],
+      defaultColumn: [
+        {
+          label: "姓名",
+          value: "Name",
+          width: 120,
+          fixed: false
+        },
+        {
+          label: "学号",
+          value: "Student_Number",
+          width: 120,
+          fixed: false
+        },
+        {
+          label: "专业",
+          value: "Major",
+          width: 120,
+          fixed: false
+        },
+        {
+          label: "年级",
+          value: "Enrollment_Year",
+          width: 120,
+          fixed: false
+        },
+        {
+          label: "性别",
+          value: "Sex",
+          width: 120,
+          fixed: false
+        }
+      ],
+      tableData: []
     };
   }
 };
