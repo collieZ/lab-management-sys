83a44f84a92127b7d04a848f1539cd031cf8b0dd
Zhiwei Zhou <zhiwei.zhou@ucchip.com>
2020-05-15 15:53:10 +0800

    update

--- src/views/user-management/index.vue
+++ src/views/user-management/index.vue
@@ -2,7 +2,12 @@
   <div class="user-container">
     <el-card shadow="always">
       <el-tabs :tab-position="tabPosition" style="height: auto">
-        <el-tab-pane v-permission="['admin']" v-for="p of tabPaneList" :key="p" :label="p">用户管理</el-tab-pane>
+        <el-tab-pane
+          v-for="p of tabPaneList"
+          :key="p"
+          v-permission="p.acl"
+          :label="p.label"
+        >{{ p.label }}</el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -14,7 +19,20 @@ export default {
   data() {
     return {
       tabPosition: "left",
-      tabPaneList: ["用户注册管理", "成员管理", "管理员管理"]
+      tabPaneList: [
+        {
+          label: "用户注册管理",
+          acl: ["super_admin", "admin"]
+        },
+        {
+          label: "管理员管理",
+          acl: ["super_admin"]
+        },
+        {
+          label: "成员管理",
+          acl: ["super_admin", "admin"]
+        }
+      ]
     };
   }
 };
